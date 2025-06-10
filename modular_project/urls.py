97b9b9775af732d1c37eb46725211e9737b487a0
darkevo24 <timotius_albert@yahoo.com>
2025-06-10 17:18:54 +0700

    update logout

--- modular_project/urls.py
+++ modular_project/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('module/', include('module_engine.urls')),
     path('products/', include('product_module.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
